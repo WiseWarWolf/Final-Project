bdf0cb33fe0062dadf72db19c8b16f6a0806595b
Kole Johnson <flamingcoder05@gmail.com>
2025-05-03 02:21:26 -0500

    got the sign in to work with the password and username

--- Scripts/signinScript.js
+++ Scripts/signinScript.js
@@ -6,26 +6,58 @@ document.addEventListener('DOMContentLoaded', function() {
  
   const signInButton = document.getElementById('signInButton');
 
-  signInInButton.addEventListener('click', signIn);
- 
+  signInButton.addEventListener('click', handleSignIn);
 
-  function signIn() {
-    const storedUser = localStorage.getItem("username");
-    const storedPass = localStorage.getItem("password");
+  function handleSignIn() {
+    validateSignIn();
+  }
 
-    const username = usernameInput.value.trim();
-    const password = passwordInput.value.trim();
+  function validateSignIn() {
+    const un = usernameInput.value.trim();
+    const pw = passwordInput.value.trim();
 
-    if (username === '' || password === '') {
-        alert('Please enter your name before clocking in.');
+    const listData = localStorage.getItem('credentials');
+    let items;
+    if (listData) {
+      items = JSON.parse(listData);
+    } else {
+      items = [];
+    }    
+
+    let recordElement;
+    const recentItems = items.slice();
+    recentItems.forEach(record => {
+      recordElement = record;
+    });
+
+    if (un === '' || pw === '') {
+        alert('Please enter your username  or password before entering the clocked-in website.');
         return;
       }
 
-    if (username === storedUser && password === storedPass) {
+    if (un == recordElement.username && pw == recordElement.password ) {
         messageText.textContent = "Login successful!"
+        setTimeout(() => {
+            window.location.href = "clocked-in.html"
+        }, 5000);
+
     } else {
         messageText.textContent = "Password or Username is invaild."
     }
+
+  }
+
+  function saveSignIn(uname, pword) {
+
+    const signInRecord = {
+      username: uname,
+      password: pword
+    };
+
+    let records = JSON.parse(localStorage.getItem('credentials')) || [];
+    records.push(signInRecord);
+    localStorage.setItem('credentials', JSON.stringify(records));
+  
   }
 
 });
